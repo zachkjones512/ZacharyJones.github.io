778b8f549f9ddc848036a4e46f0a9cd7721fd17f
zachkjones512 <zachkjones512@gmail.com>
2026-02-08 16:38:39 -0600

    Fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ZacharyJones.github.io',
+  base: '/',
   plugins: [react()],
 })
